e086f758acd819b44484033fb723bdceaf7c9cec
Shoeb Tamboli <shoeb.tamboli@tesa.com>
2021-12-25 15:38:51 +0100

    Updates

---
+++ precache-manifest.05f78290989c1fb7a2edb90899230104.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb453bec8a5a2e6a0e4916e1297cff21",
+    "url": "/smartRecruiterApp/index.html"
+  },
+  {
+    "revision": "93639e607c864ad89e65",
+    "url": "/smartRecruiterApp/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "82d1bd64b30f983beefb",
+    "url": "/smartRecruiterApp/static/css/main.4cbd0370.chunk.css"
+  },
+  {
+    "revision": "93639e607c864ad89e65",
+    "url": "/smartRecruiterApp/static/js/2.ca12766e.chunk.js"
+  },
+  {
+    "revision": "6020e2578ce61e153ee5130f44d173fc",
+    "url": "/smartRecruiterApp/static/js/2.ca12766e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "82d1bd64b30f983beefb",
+    "url": "/smartRecruiterApp/static/js/main.3acc8522.chunk.js"
+  },
+  {
+    "revision": "189e64effd07308243e2",
+    "url": "/smartRecruiterApp/static/js/runtime-main.1ad93508.js"
+  }
+]);
